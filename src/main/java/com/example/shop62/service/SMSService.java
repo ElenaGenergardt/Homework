d8d02f1c7ab686cc08e0396a88acc0b935ab3d06
ElenaGenergardt <P_L_Gen@web.de>
2026-02-09 21:46:53 +0100

    Add homework 3

--- src/main/java/com/example/shop62/service/SMSService.java
+++ src/main/java/com/example/shop62/service/SMSService.java
@@ -1,3 +1,14 @@
 package com.example.shop62.service;
 
+import org.springframework.stereotype.Component;
 
+@Component
+public class SMSService implements MessageService{
+
+    @Override
+    public void sendMessage(String message, String recipient) {
+        System.out.println("Sending SMS to " + recipient);
+        System.out.println("Message: " + message);
+
+    }
+}
